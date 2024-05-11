49f92f1e8c88c202aab83902efc47aaaabc23b58
CodeDotJS <rushforlinux@gmail.com>
2024-05-11 19:33:46 +0530

    return `userinfo` & update `headers`

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:125.0) Gecko/20100101 Firefox/125.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Referer': 'https://github.com/',
+    'Connection': 'keep-alive',
+}
+
 @app.route('/fetch', methods=['POST'])
 def first_push():
     repository = request.json.get('repository')
@@ -15,36 +24,58 @@ def first_push():
         return jsonify({'error': 'Repository not provided in the request.'}), 400
 
     github_url = f"https://github.com/{repository}"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    # headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(github_url, headers=headers)
 
     if response.status_code == 200:
         total_commits = response.text.split('commitCount":"')[1].split('"')[0]
         commit_count = normalize(total_commits)
         page = math.ceil(commit_count / 30)
-        api_url = f"https://api.github.com/repos/{repository}/commits?page={page}"
-        api_response = requests.get(api_url, headers=headers)
+
+        with requests.Session() as session:
+            api_url = f"https://api.github.com/repos/{repository}/commits?page={page}"
+            api_response = session.get(api_url, headers=headers)
+
+            profile_url = f"https://api.github.com/users/{repository.split('/')[0]}"
+            profile_response = session.get(profile_url, headers=headers)
 
         if api_response.status_code == 200:
             commits_data = api_response.json()
+            user_data = profile_response.json()
             if commits_data:
                 first_commit = commits_data[-1]
-                return jsonify({
+                data = {
+                "user": user(user_data),
+                "commit": {
                     "repository": github_url,
-                    "avatar": first_commit["author"]["avatar_url"],
-                    "profile": first_commit["author"]["html_url"],
-                    "username": first_commit["author"]["login"],
                     "date": first_commit["commit"]["author"]["date"],
-                    "name": first_commit["commit"]["author"]["name"],
                     "message": first_commit["commit"]["message"],
                     "commit": first_commit["html_url"]
-                })
+                    }
+                }
+                return jsonify(data)
             else:
                 return jsonify({'error': 'No commits found for the repository.'}), 404
         else:
             return jsonify({'error': f"Error fetching data from GitHub API: {api_response.status_code}"}), 500
     else:
         return jsonify({'error': f"Error fetching data from GitHub: {response.status_code}"}), 500
+
+def user(user_data):
+    return {
+        "avatar": user_data["avatar_url"],
+        "bio": user_data["bio"],
+        "blog": user_data["blog"],
+        "followers": user_data["followers"],
+        "following": user_data["following"],
+        "id": user_data["id"],
+        "location": user_data["location"],
+        "username": user_data["login"],
+        "name": user_data["name"],
+        "repositories": user_data["public_repos"],
+        "twitter": user_data["twitter_username"],
+        "type": user_data["type"]
+    }
 
 def normalize(total_commits):
     try:
